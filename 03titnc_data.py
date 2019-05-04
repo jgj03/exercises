be43f7c06fc3dbf2e42ea86606acd1ea10ae2d01
jgj03 <50008646+jgj03@users.noreply.github.com>
2019-05-04 15:47:29 +0530

    Add files via upload

---
+++ 03titnc_data.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""" @author: JGJ """
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+titan = sns.load_dataset('titanic')
+titan.info()
+titan.describe()
+titan.head()
+titan['survived'].unique()
+titan.survived.unique()
+titan.columns
+
+#Check the survial rate by gender
+titan.groupby('sex')[['survived']]
+titan.groupby('sex')[['survived']].mean()
+
+titan.columns
+titan.class.unique()# ERROR : SyntaxError: invalid syntax
+titan['class'].unique()
+
+#check the gender and class based survival rate
+titan.groupby(['sex', 'class'])['survived'].aggregate('mean')
+titan.groupby(['sex', 'class'])['survived'].aggregate('mean').unstack()
+
+#Same operation as above using pivot_table
+titan.pivot_table('survived', index='sex', columns='class')
+titan.pivot_table('survived', index='class', columns='sex')
+
+#Multi level pivot
+age = pd.cut(titan['age'], [0, 18, 80])
+titan.pivot_table('survived',['sex', age],'class')
+
+titan.fare.unique()
+fare = pd.qcut(titan['fare'],2)
+titan.pivot_table('survived', ['sex',age],[fare, 'class'])
+
+#Additional pivot_table options
+titan.pivot_table('survived', index='sex', columns='class', margins=True)
